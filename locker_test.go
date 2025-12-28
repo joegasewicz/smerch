add379275db233a6cb62abac96d6795764f695d9
joegasewicz <joegasewicz@gmail.com>
2025-12-28 20:17:08 +0000

    #4

--- locker_test.go
+++ locker_test.go
@@ -1,1 +1,14 @@
 package smerch_test
+
+import (
+	"testing"
+
+	"github.com/joegasewicz/smerch"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestLocker_IsLocked(t *testing.T) {
+	locker := smerch.NewLocker("examples/smerch.lock")
+	result := locker.IsLocked()
+	assert.Equal(t, result, true)
+}
